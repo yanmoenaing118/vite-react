283b46cf077ee4bfdebc2e29dae7224012a53134
yanmoenaing118 <yan.webdev.1234@gmail.com>
2022-03-17 17:14:52 +0630

    swiper

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,94 @@
-import { Outlet, Link } from "react-router-dom";
-import "./App.css";
-import useAuth from "./lib/useAuth";
+import { Swiper, SwiperSlide, useSwiper } from "swiper/react";
+import SwiperCore, { Pagination } from "swiper";
 
-export default function App() {
-  const { user, handleLogin, handleLogout } = useAuth();
+import { useState, useEffect } from "react";
+
+import "swiper/css";
+import "swiper/css/pagination";
+import "swiper/css/pagination";
+
+import "./App.css";
+
+SwiperCore.use([Pagination]);
+
+export default () => {
+  const [swiper, setSwiper] = useState(null);
+
+  useEffect(() => {
+    console.log("swiper instance", swiper);
+  }, [swiper]);
+
+  const slideNext = () => swiper && swiper.slideNext();
+  const slidePrev = () => swiper && swiper.slidePrev();
 
   return (
-    <div>
-      <h1>My app</h1>
-      <nav
-        style={{
-          borderBottom: "solid 1px",
-          paddingBottom: "1rem",
+    <section>
+      <Swiper
+        spaceBetween={18}
+        slidesPerView={1}
+        pagination={{
+          el: ".swiper-pagination",
+          type: "bullets",
+          clickable: true
         }}
+        modules={[Pagination]}
+        onSlideChange={() => console.log("slide change")}
+        onSwiper={(swiper) => setSwiper(swiper)}
       >
-        <Link to="/invoices">Invoices</Link> |{" "}
-        <Link to="/expenses">Expenses</Link> |{" "}
-        {user ? (
-          <>
-            <Link to="/profile">{user.name}</Link> |{" "}
-            <button onClick={handleLogout} style={{ padding: ".2em .5em" }}>
-              Log out
-            </button>
-          </>
-        ) : (
-          <Link to="/login">Login</Link>
-        )}
-      </nav>
-      <Outlet />
-      <footer style={{ padding: "10rem", backgroundColor: "#444" }}></footer>
-    </div>
+        <SlidePrevButton />
+        <SwiperSlide>Slide 1</SwiperSlide>
+        <SwiperSlide>Slide 2</SwiperSlide>
+        <SwiperSlide>Slide 3</SwiperSlide>
+        <SwiperSlide>Slide 4</SwiperSlide>
+        <SlideNextButton />
+        <div className="swiper-pagination"></div>
+      </Swiper>
+    </section>
+  );
+};
+
+function SlideNextButton() {
+  const swiper = useSwiper();
+
+  const next = () => {
+    console.log(swiper);
+    swiper.slideNext();
+  };
+
+  return (
+    <button
+      onClick={next}
+      style={{
+        position: "absolute",
+        right: "0",
+        top: "50%",
+        zIndex: "10",
+      }}
+    >
+      next
+    </button>
+  );
+}
+
+function SlidePrevButton() {
+  const swiper = useSwiper();
+
+  const next = () => {
+    console.log(swiper);
+    swiper.slidePrev();
+  };
+
+  return (
+    <button
+      onClick={next}
+      style={{
+        position: "absolute",
+        left: "0",
+        top: "50%",
+        zIndex: "10",
+      }}
+    >
+      prev
+    </button>
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,54 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import Expenses from "./routes/expenses";
-import Invoices from "./routes/invoices";
-import Invoice from "./routes/invoice";
-import Profile from "./routes/profile";
-import Login from "./routes/login";
-import RequireAuth from "./RequireAuth";
-import StoreProvider from "./store";
 
 ReactDOM.render(
   <React.StrictMode>
-    <StoreProvider>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<App />}>
-            <Route path="login" element={<Login />} />
-            <Route path="expenses" element={<Expenses />} />
-            <Route path="invoices" element={<Invoices />}>
-              <Route
-                index
-                element={
-                  <main style={{ padding: "1rem" }}>
-                    <p>Select an invoice</p>
-                  </main>
-                }
-              />
-              <Route path=":invoiceId" element={<Invoice />} />
-            </Route>
-            <Route
-              path="profile"
-              element={
-                <RequireAuth>
-                  <Profile />
-                </RequireAuth>
-              }
-            />
-            <Route
-              path="*"
-              element={
-                <main style={{ padding: "1rem" }}>
-                  <p>There's nothing here!</p>
-                </main>
-              }
-            ></Route>
-          </Route>
-        </Routes>
-      </BrowserRouter>
-    </StoreProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById("root")
 );
